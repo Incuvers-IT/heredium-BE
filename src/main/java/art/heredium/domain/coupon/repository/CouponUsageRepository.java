547ee8408b3ce9af39de13c740e7d39838a8ddb0
Sotatek-KimMai <kim.mai@sotatek.com>
2024-10-02 08:33:38 +0700

    IH-30 Rename UserCoupon to CouponUsage

--- src/main/java/art/heredium/domain/coupon/repository/CouponUsageRepository.java
+++ src/main/java/art/heredium/domain/coupon/repository/CouponUsageRepository.java
@@ -3,7 +3,7 @@ package art.heredium.domain.coupon.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import art.heredium.domain.coupon.entity.UserCoupon;
+import art.heredium.domain.coupon.entity.CouponUsage;
 
 @Repository
-public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {}
+public interface CouponUsageRepository extends JpaRepository<CouponUsage, Long> {}
