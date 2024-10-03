c595cb29ab9102bd7b8118e0e9af92d878fbff37
Sotatek-HungNgo4 <hung.ngo4@sotatek.com>
2024-10-03 17:37:51 +0700

    IH-24 Implement NICE Payment cancel payment API client

---
+++ src/main/java/art/heredium/payment/nicepayments/dto/response/NicePaymentsBaseResponse.java
@@ -0,0 +1,93 @@
+package art.heredium.payment.nicepayments.dto.response;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class NicePaymentsBaseResponse {
+  private String resultCode;
+  private String resultMsg;
+  private String tid;
+  private String cancelledTid;
+  private String orderId;
+  private String ediDate;
+  private String signature;
+  private String status;
+  private String paidAt;
+  private String failedAt;
+  private String cancelledAt;
+  private String payMethod;
+  private Integer amount;
+  private Integer balanceAmt;
+  private String goodsName;
+  private String mallReserved;
+  private Boolean useEscrow;
+  private String currency;
+  private String channel;
+  private String approveNo;
+  private String buyerName;
+  private String buyerTel;
+  private String buyerEmail;
+  private String receiptUrl;
+  private String mallUserId;
+  private Boolean issuedCashReceipt;
+  private Coupon coupon;
+  private Card card;
+  private VBank vbank;
+  private Bank bank;
+  private String cellphone;
+  private List<Cancel> cancels;
+  private String cashReceipts;
+  private String messageSource;
+
+  @Getter
+  @Setter
+  private static class Card {
+    private String cardCode;
+    private String cardName;
+    private String cardNum;
+    private Integer cardQuota;
+    private Boolean isInterestFree;
+    private String cardType;
+    private Boolean canPartCancel;
+    private String acquCardCode;
+    private String acquCardName;
+  }
+
+  @Getter
+  @Setter
+  private static class Coupon {
+    private Integer couponAmt;
+  }
+
+  @Getter
+  @Setter
+  private static class VBank {
+    private String vbankCode;
+    private String vbankName;
+    private String vbankNumber;
+    private String vbankExpDate;
+    private String vbankHolder;
+  }
+
+  @Getter
+  @Setter
+  private static class Bank {
+    private String bankCode;
+    private String bankName;
+  }
+
+  @Getter
+  @Setter
+  private static class Cancel {
+    private String tid;
+    private Integer amount;
+    private String cancelledAt;
+    private String reason;
+    private String receiptUrl;
+    private Integer couponAmt;
+  }
+}
--- src/main/java/art/heredium/payment/nicepayments/feign/client/NicePaymentsClient.java
+++ src/main/java/art/heredium/payment/nicepayments/feign/client/NicePaymentsClient.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 import art.heredium.payment.nicepayments.dto.request.NicePaymentsPayRequest;
+import art.heredium.payment.nicepayments.dto.request.NicePaymentsRefundRequest;
 import art.heredium.payment.nicepayments.dto.response.NicePaymentsPayResponse;
+import art.heredium.payment.nicepayments.dto.response.NicePaymentsRefundResponse;
 import art.heredium.payment.nicepayments.feign.config.NicePaymentsFeignConfig;
 
 @FeignClient(
@@ -21,4 +23,10 @@ public interface NicePaymentsClient {
       @RequestHeader("Authorization") String authorization,
       @PathVariable("tid") String tid,
       @RequestBody NicePaymentsPayRequest request);
+
+  @PostMapping("/v1/payments/{tid}/cancel")
+  NicePaymentsRefundResponse refund(
+      @RequestHeader("Authorization") String authorization,
+      @PathVariable("tid") String tid,
+      @RequestBody NicePaymentsRefundRequest request);
 }
