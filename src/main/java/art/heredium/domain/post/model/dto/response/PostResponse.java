cd7cfa3f6e16aa37b930a0e623893b78e0b5d57f
Sotatek-KimMai <kim.mai@sotatek.com>
2024-10-04 11:16:31 +0700

    IH-39 Add json property

--- src/main/java/art/heredium/domain/post/model/dto/response/PostResponse.java
+++ src/main/java/art/heredium/domain/post/model/dto/response/PostResponse.java
@@ -4,14 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class PostResponse {
+
   private Long id;
+
   private String name;
+
+  @JsonProperty("image_url")
   private String imageUrl;
+
+  @JsonProperty("is_enabled")
   private Boolean isEnabled;
+
+  @JsonProperty("content_detail")
   private String contentDetail;
+
+  @JsonProperty("navigation_link")
   private String navigationLink;
 }
