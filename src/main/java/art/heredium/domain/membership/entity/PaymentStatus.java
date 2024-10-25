37c8265a9440d91eb9b3356ada99b78cf35109c6
Sotatek-KimMai <kim.mai@sotatek.com>
2024-10-25 10:12:43 +0700

    IH-117 Use Korean language for PaymentStatus data

--- src/main/java/art/heredium/domain/membership/entity/PaymentStatus.java
+++ src/main/java/art/heredium/domain/membership/entity/PaymentStatus.java
@@ -1,11 +1,14 @@
 package art.heredium.domain.membership.entity;
 
+import lombok.Getter;
+
 public enum PaymentStatus {
   COMPLETED("완전한"),
   REFUND("환불하다"),
   EXPIRED("만료됨"),
   ;
 
+  @Getter
   private String desc;
 
   PaymentStatus(final String desc) {
