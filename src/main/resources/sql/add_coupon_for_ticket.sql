06e508d04fb6c92817443b663354056523890699
ThoLe-Sotatek <tho.le@sotatek.com>
2024-11-01 18:36:26 +0700

    IH-176 Update sql

--- src/main/resources/sql/add_coupon_for_ticket.sql
+++ src/main/resources/sql/add_coupon_for_ticket.sql
@@ -1,2 +1,2 @@
 ALTER TABLE ticket ADD COLUMN coupon_uuid VARCHAR(255);
-ALTER TABLE ticket ADD COLUMN is_coupon_already_refund BOOLEAN;
+ALTER TABLE ticket ADD COLUMN is_coupon_already_refund BOOLEAN not null default false;
