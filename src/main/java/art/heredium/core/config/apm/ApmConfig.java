553701b13a39c1a35ebaf5487b318058d220849e
Sotatek-HungNgo4 <hung.ngo4@sotatek.com>
2024-11-05 12:04:53 +0700

    Implement Elastic Stack

---
+++ src/main/java/art/heredium/core/config/apm/ApmConfig.java
@@ -0,0 +1,31 @@
+package art.heredium.core.config.apm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import co.elastic.apm.attach.ElasticApmAttacher;
+
+@Component
+public class ApmConfig {
+  private static final String SERVER_URL_KEY = "server_url";
+  private static final String SERVICE_NAME_KEY = "service_name";
+  private static final String ENVIRONMENT_KEY = "environment";
+  private static final String APPLICATION_PACKAGES_KEY = "application_packages";
+  private static final String LOG_LEVEL_KEY = "log_level";
+  private static final String TRANSACTION_SAMPLE_RATE = "transaction_sample_rate";
+
+  @Bean
+  public void inItApmProperties() {
+    Map<String, String> apmProperties = new HashMap<>();
+    apmProperties.put(SERVER_URL_KEY, "http://localhost:8200");
+    apmProperties.put(SERVICE_NAME_KEY, "heredium-staging");
+    apmProperties.put(ENVIRONMENT_KEY, "apm-heredium");
+    apmProperties.put(APPLICATION_PACKAGES_KEY, "art.heredium.*");
+    apmProperties.put(LOG_LEVEL_KEY, "INFO");
+    apmProperties.put(TRANSACTION_SAMPLE_RATE, "1.0");
+    ElasticApmAttacher.attach(apmProperties);
+  }
+}
