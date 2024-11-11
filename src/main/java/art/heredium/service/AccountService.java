20658be160a743e275b0ba27c89dabc9fe243043
Sotatek-KimMai <kim.mai@sotatek.com>
2024-11-11 10:46:09 +0700

    IH-194 Fix column validator

--- src/main/java/art/heredium/service/AccountService.java
+++ src/main/java/art/heredium/service/AccountService.java
@@ -431,7 +431,7 @@ public class AccountService {
       final String expectedColumnName = CouponIssuanceTemplateColumns.getColumnNameByIndex(i);
       if (!expectedColumnName.equalsIgnoreCase(StringUtils.trim(columnName))) {
         throw new ApiException(
-            ErrorCode.INVALID_EXCEL_COLUMNS, "Column names should be ['이메일', '핸드폰', '이름']");
+            ErrorCode.INVALID_EXCEL_COLUMNS, "Column names should be ['계정', '이름', '연락처']");
       }
     }
   }
