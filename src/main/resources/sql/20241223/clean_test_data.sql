737a2aee27cf9f3bf504b48215455829ca2e0331
Sotatek-KimMai <kim.mai@sotatek.com>
2024-12-23 13:37:25 +0700

    IH-245 Clean data test in ticket log and ticket price

--- src/main/resources/sql/20241223/clean_test_data.sql
+++ src/main/resources/sql/20241223/clean_test_data.sql
@@ -1,6 +1,14 @@
 -- Delete related coupon_usage records
-DELETE cu FROM coupon_usage cu
-INNER JOIN membership_registration mr ON cu.account_id = mr.account_id;
+DELETE FROM coupon_usage;
+
+-- Delete related ticket records (including child tables first)
+DELETE tl FROM ticket_log tl
+INNER JOIN ticket t ON tl.ticket_id = t.id
+INNER JOIN membership_registration mr ON t.account_id = mr.account_id;
+
+DELETE tp FROM ticket_price tp
+INNER JOIN ticket t ON tp.ticket_id = t.id
+INNER JOIN membership_registration mr ON t.account_id = mr.account_id;
 
 -- Delete related ticket records
 DELETE t FROM ticket t
