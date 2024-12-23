b737418933425c69e715991c070797876e6d6ee7
Sotatek-KimMai <kim.mai@sotatek.com>
2024-12-23 11:19:50 +0700

    IH-245 Write sql script to clean test data

---
+++ src/main/resources/sql/20241223/clean_test_data.sql
@@ -0,0 +1,18 @@
+-- Delete related coupon_usage records
+DELETE FROM coupon_usage;
+
+-- Delete related ticket records (including child tables first)
+DELETE tl FROM ticket_log tl
+INNER JOIN ticket t ON tl.ticket_id = t.id
+INNER JOIN membership_registration mr ON t.account_id = mr.account_id;
+
+DELETE tp FROM ticket_price tp
+INNER JOIN ticket t ON tp.ticket_id = t.id
+INNER JOIN membership_registration mr ON t.account_id = mr.account_id;
+
+-- Delete related ticket records
+DELETE t FROM ticket t
+INNER JOIN membership_registration mr ON t.account_id = mr.account_id;
+
+-- Delete all membership_registration records
+DELETE FROM membership_registration;
