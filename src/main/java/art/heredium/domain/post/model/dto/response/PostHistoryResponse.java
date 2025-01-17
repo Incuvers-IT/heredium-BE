529b97755ca85ee13562928926aa80401e70cca6
Sotatek-KimMai <kim.mai@sotatek.com>
2025-01-17 16:16:05 +0700

    IH-254 Fix comments

--- src/main/java/art/heredium/domain/post/model/dto/response/PostHistoryResponse.java
+++ src/main/java/art/heredium/domain/post/model/dto/response/PostHistoryResponse.java
@@ -7,13 +7,26 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @Builder
 public class PostHistoryResponse {
+
+  @JsonProperty("post_history_id")
   private Long postHistoryId;
-  private AdminPostDetailsResponse content;
+
+  @JsonProperty("content")
+  private String content;
+
+  @JsonProperty("modified_date")
   private LocalDateTime modifiedDate;
+
+  @JsonProperty("modify_user_email")
   private String modifyUserEmail;
+
+  @JsonProperty("modify_user_name")
+  private String modifyUserName;
 }
